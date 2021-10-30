946db9809f6c1e55e577bc9495bb5ea8d06dfde4
Caleb Froese <caleb.froese@gmail.com>
2021-10-30 14:44:18 +1030

    Fixed timing

--- src/main/java/com/calebfroese/dockerbungee/DockerSpigotPlugin.java
+++ src/main/java/com/calebfroese/dockerbungee/DockerSpigotPlugin.java
@@ -1,5 +1,6 @@
 package com.calebfroese.dockerbungee;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.collect.Iterables;
@@ -11,17 +12,21 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-import de.codingair.codingapi.tools.Location;
-import de.codingair.warpsystem.api.destinations.utils.IDestination;
 import de.codingair.warpsystem.api.events.AsyncPlayerTeleportEvent;
 
-public class DockerSpigotPlugin extends JavaPlugin implements Listener
-, PluginMessageListener 
-{
-  String serverName = null;
+public class DockerSpigotPlugin extends JavaPlugin implements Listener, PluginMessageListener {
+  ArrayList<String> onlineServerNames = new ArrayList<String>();
+  // Whether we are currently pending a response from Bungeecord about the online
+  // servers
+  // This avoids having multiple players join in quick succession causing multiple
+  // requests which could finish at different times and clear onlineServerNames
+  // We save the player name, as bungeecord messages are sent along with an online
+  // player. If that playere disconnects, clear it, as we will have lost the connection.
+  String isPendingOnlineServerRequest = null;
 
   @Override
   public void onEnable() {
@@ -41,81 +46,86 @@ public class DockerSpigotPlugin extends JavaPlugin implements Listener
 
   @Override
   public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-    getLogger().info("onPluginMessageReceived");
     if (!channel.equals("dockerbungee:main")) {
-      getLogger().info("REreceived a mesage for some other chabnnel" + channel);
       return;
     }
     ByteArrayDataInput in = ByteStreams.newDataInput(message);
     in.readUTF();
     String subchannel = in.readUTF();
-    getLogger().info("Subchannel" + subchannel);
     if (subchannel.equals("ServerOnline")) {
-      this.serverName = in.readUTF();
-      getLogger().info("A NEW SERVER IS ONLINE! WOOHOO:: " + this.serverName);
+      // The online servers string arrives as a comma separated list, e.g.
+      // "server1,server2,server3"
+      String onlineServers = in.readUTF();
+      getLogger().info("Online servers: " + onlineServers);
+      for (String serverName : onlineServers.split(",")) {
+        this.onlineServerNames.add(serverName);
+      }
+      this.isPendingOnlineServerRequest = null;
     }
   }
 
   @EventHandler
   public void onWarp(AsyncPlayerTeleportEvent e) {
-    getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onWarp");
-    // e.setCancelled(true);
-    this.requestTeleportToServer("another");
-    
-    while (!this.serverName.equalsIgnoreCase("another")) {
-      try {
-        TimeUnit.SECONDS.sleep(1);
-        getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onWarpnope......");
-      } catch (InterruptedException e1) {
-        // TODO Auto-generated catch block
-        getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onWarperror......");
-        e1.printStackTrace();
-      }
+    if (e.getOptions().getDestination().getTargetServer() == null) {
+      getLogger().info("Target server is null, unable to teleport");
+      return;
     }
-    getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onWarp CONTINNUNIGINEGINEGIEN");
-    // IDestination destination = e.getOptions().getDestination();
-    // Location location = destination.buildLocation();
-    // if (location != null && inSpecificArea(location)) {
-    // e.setCancelled(true);
-    // e.getPlayer().sendMessage("§cWarps §8» §7This area is §ccurrently
-    // unavailable§7.");
-    // }
+    getLogger().info("Requesting to warp " + e.getPlayer().getName() + " to "
+        + e.getOptions().getDestination().getTargetServer() + "...");
+    waitForServer(e.getOptions().getDestination().getTargetServer());
+    getLogger().info("Sending warp " + e.getPlayer().getName() + " to "
+        + e.getOptions().getDestination().getTargetServer() + "...");
   }
+
+  // @EventHandler
+  // public void onTeleport(AsyncPlayerTeleportEvent e) {
+  //   if (e.getOptions().getDestination().getTargetServer() == null) {
+  //     getLogger().info("Target server is null, unable to teleport");
+  //     return;
+  //   }
+  //   getLogger().info("Requesting to teleport " + e.getPlayer().getName() + " to "
+  //       + e.getOptions().getDestination().getTargetServer() + "...");
+  //   waitForServer(e.getOptions().getDestination().getTargetServer());
+  //   getLogger().info("LETS GO");
+  // }
 
   @EventHandler
-  public void onTeleport(AsyncPlayerTeleportEvent e) {
-    getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onTeleport");
-    // e.setCancelled(true);
-    this.requestTeleportToServer("another");
-    
-    while (!this.serverName.equalsIgnoreCase("another")) {
+  public void onPlayerDisconnect(PlayerQuitEvent e) {
+    getLogger().info("Player has disconnected!");
+    if (this.isPendingOnlineServerRequest != null && this.isPendingOnlineServerRequest == e.getPlayer().getName()) {
+      getLogger().info("It was hee!");
+      this.isPendingOnlineServerRequest = null;
+    }
+  }
+
+  private void waitForServer(String serverName) {
+    // We have to clear and request the teleport every time as we don't know for
+    // sure if a server goes down.
+    this.bootAndJoinServerRequest(serverName);
+
+    while (!this.onlineServerNames.contains(serverName)) {
       try {
         TimeUnit.SECONDS.sleep(1);
-        getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent  onTeleportnope......");
       } catch (InterruptedException e1) {
-        // TODO Auto-generated catch block
-        getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onTeleporterror......");
         e1.printStackTrace();
       }
     }
-    getLogger().info("AsyncPlayerTeleportEvent AsyncPlayerTeleportEvent onTeleport CONTINNUNIGINEGINEGIEN");
-    // this.requestTeleportToServer("another");
-    // IDestination destination = e.getOptions().getDestination();
-    // String targetServer = destination.getTargetServer();
-
-    // boolean onSameServer = targetServer == null;
-    // if (onSameServer) return;
-
-    // if (isOffline(targetServer)) startSync(targetServer);
   }
 
-  private void requestTeleportToServer(String serverName) {
-    ByteArrayDataOutput out = ByteStreams.newDataOutput();
-    out.writeUTF("RequestTeleport");
-    out.writeUTF(serverName);
+  private void bootAndJoinServerRequest(String serverName) {
+    if (this.isPendingOnlineServerRequest != null) {
+      getLogger().info("Not sending teleport request as there is one pending!");
+      return;
+    }
+    getLogger().info("Sending teleport request!");
 
     // If you don't care about the player
     Player player = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
+    this.isPendingOnlineServerRequest = player.getName();
+    this.onlineServerNames.clear();
+    ByteArrayDataOutput out = ByteStreams.newDataOutput();
+    out.writeUTF("BootAndJoinServer");
+    out.writeUTF(serverName);
 
     player.sendPluginMessage(this, "dockerbungee:main", out.toByteArray());
   }
